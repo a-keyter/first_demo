907860668c8a375ae8d6a90d7ee7a0949a133399
a-keyter <106679998+a-keyter@users.noreply.github.com>
2024-05-27 21:59:32 +0100

    Added Supabase Auth

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,34 +1,59 @@
-import Link from 'next/link';
-import React from 'react'
-import { Button } from './ui/button';
- 
-function NavBar() {
-    const NavLinks = [
-      {
-        name: "Home",
-        href: "/" 
-      },
-      {
-        name: "Contact",
-        href: "/contact" 
-      },
-      {
-        name: "Signup",
-        href: "/signup"
-      }
-    ];
-  
-    return (
-      <div className='w-full flex justify-end gap-x-4 py-2'>
-        {NavLinks.map((link) => (
-          <Link key={link.name} href={link.href} style={{ textDecoration: 'none' }}>
-            <Button>
-              {link.name}
-            </Button>
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import SignOut from "./SignOut";
+import { createClient } from "@/utils/supabase/server";
+
+interface User {
+  id?: string;
+  email?: string;
+}
+
+const fetchUser = async (): Promise<User | null> => {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  return user ? { id: user.id, email: user.email } : null;
+};
+
+export default async function NavBar() {
+  const user = await fetchUser();
+
+  const NavLinks = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "Contact",
+      href: "/contact",
+    },
+  ];
+
+  return (
+    <div className="w-full flex justify-end gap-x-4 py-2 pr-4">
+      {NavLinks.map((link) => (
+        <Link
+          key={link.name}
+          href={link.href}
+          style={{ textDecoration: "none" }}
+        >
+          <Button>{link.name}</Button>
+        </Link>
+      ))}
+      {user ? (
+        <div className="flex gap-x-4">
+          <Link href="/dashboard">
+            <Button>Dashboard</Button>
           </Link>
-        ))}
-      </div>
-    );
-  }
-  
-  export default NavBar;
+          <SignOut />
+        </div>
+      ) : (
+        <Link href="/login" style={{ textDecoration: "none" }}>
+          <Button>Login</Button>
+        </Link>
+      )}
+    </div>
+  );
+}
